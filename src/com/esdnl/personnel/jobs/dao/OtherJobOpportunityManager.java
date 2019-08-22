fc7c809d1486706ede2cc04560ddf55db2919f6e
Chris Crane <chriscrane@nlesd.ca>
2019-08-22 10:02:30 -0230

    Fixed NPE

--- src/com/esdnl/personnel/jobs/dao/OtherJobOpportunityManager.java
+++ src/com/esdnl/personnel/jobs/dao/OtherJobOpportunityManager.java
@@ -1,80 +1,38 @@
 package com.esdnl.personnel.jobs.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.Properties;
 import java.util.TreeMap;
 import java.util.Vector;
 
-import oracle.jdbc.OracleCallableStatement;
-import oracle.jdbc.OracleTypes;
-
+import com.awsd.mail.bean.AlertBean;
+import com.awsd.school.bean.RegionBean;
+import com.awsd.school.bean.RegionException;
 import com.awsd.school.dao.RegionManager;
-import com.awsd.servlet.ControllerServlet;
 import com.esdnl.dao.DAOUtils;
 import com.esdnl.personnel.jobs.bean.JobOpportunityException;
 import com.esdnl.personnel.jobs.bean.OtherJobOpportunityBean;
 import com.esdnl.personnel.jobs.constants.PostingType;
 
+import oracle.jdbc.OracleCallableStatement;
+import oracle.jdbc.OracleTypes;
+
 public class OtherJobOpportunityManager {
-
-	private static String DATA_WAREHOUSE_JDBC_URL;
-	private static String DATA_WAREHOUSE_JDBC_USERNAME;
-	private static String DATA_WAREHOUSE_JDBC_PASSWORD;
-
-	static {
-		boolean loaded = false;
-		try {
-			Class.forName("com.awsd.servlet.ControllerServlet");
-
-			DATA_WAREHOUSE_JDBC_URL = ControllerServlet.DATA_WAREHOUSE_JDBC_URL;
-			DATA_WAREHOUSE_JDBC_USERNAME = ControllerServlet.DATA_WAREHOUSE_JDBC_USERNAME;
-			DATA_WAREHOUSE_JDBC_PASSWORD = ControllerServlet.DATA_WAREHOUSE_JDBC_PASSWORD;
-
-			loaded = true;
-
-		}
-		catch (ClassNotFoundException e) {
-			loaded = false;
-		}
-
-		if (!loaded) {
-			try {
-				InputStream istr = OtherJobOpportunityManager.class.getResourceAsStream("connection.properties");
-				Properties prop = new Properties();
-				prop.load(istr);
-
-				Class.forName(prop.getProperty("jdbc.driver"));
-
-				DATA_WAREHOUSE_JDBC_URL = prop.getProperty("jdbc.url");
-				DATA_WAREHOUSE_JDBC_USERNAME = prop.getProperty("jdbc.username");
-				DATA_WAREHOUSE_JDBC_PASSWORD = prop.getProperty("jdbc.password");
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
 	public static OtherJobOpportunityBean[] getOtherJobOpportunityBeans() throws JobOpportunityException {
 
-		Vector v_opps = null;
+		Vector<OtherJobOpportunityBean> v_opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			v_opps = new Vector(5);
+			v_opps = new Vector<OtherJobOpportunityBean>(5);
 
 			con = DAOUtils.getConnection();
 
@@ -112,17 +70,18 @@ public class OtherJobOpportunityManager {
 		return (OtherJobOpportunityBean[]) v_opps.toArray(new OtherJobOpportunityBean[0]);
 	}
 
-	public static HashMap getInternalOnlyOtherJobOpportunityBeans() throws JobOpportunityException {
+	public static HashMap<RegionBean, Vector<OtherJobOpportunityBean>> getInternalOnlyOtherJobOpportunityBeans()
+			throws JobOpportunityException {
 
-		HashMap regions = null;
-		Vector opps = null;
+		HashMap<RegionBean, Vector<OtherJobOpportunityBean>> regions = null;
+		Vector<OtherJobOpportunityBean> opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			regions = new HashMap();
+			regions = new HashMap<RegionBean, Vector<OtherJobOpportunityBean>>();
 
 			con = DAOUtils.getConnection();
 
@@ -139,9 +98,9 @@ public class OtherJobOpportunityManager {
 					continue;
 
 				if (regions.containsKey(jBean.getRegion()))
-					opps = (Vector) regions.get(jBean.getRegion());
+					opps = (Vector<OtherJobOpportunityBean>) regions.get(jBean.getRegion());
 				else {
-					opps = new Vector();
+					opps = new Vector<OtherJobOpportunityBean>();
 					regions.put(jBean.getRegion(), opps);
 				}
 				opps.add(jBean);
@@ -171,14 +130,14 @@ public class OtherJobOpportunityManager {
 
 	public static OtherJobOpportunityBean[] getClosedOtherJobOpportunityBeans() throws JobOpportunityException {
 
-		Vector v_opps = null;
+		Vector<OtherJobOpportunityBean> v_opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			v_opps = new Vector(5);
+			v_opps = new Vector<OtherJobOpportunityBean>(5);
 
 			con = DAOUtils.getConnection();
 
@@ -256,25 +215,23 @@ public class OtherJobOpportunityManager {
 			catch (Exception e) {}
 		}
 	}
-	
+
 	public static OtherJobOpportunityBean getOtherJobOpportunityBeanById(int job_id) throws JobOpportunityException {
 
-		
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			
 
 			con = DAOUtils.getConnection();
-			
+
 			stat = con.prepareCall("begin ? := awsd_user.personnel_jobs_pkg.get_other_job_opp_by_id(?); end;");
 
 			stat.registerOutParameter(1, OracleTypes.CURSOR);
 			stat.setInt(2, job_id);
-			
+
 			stat.execute();
 			rs = ((OracleCallableStatement) stat).getCursor(1);
 
@@ -301,43 +258,9 @@ public class OtherJobOpportunityManager {
 		}
 
 		return jBean;
-	
+
 	}
-	
 
-	public static OtherJobOpportunityBean createOtherJobOpportunityBean(ResultSet rs) {
-
-		OtherJobOpportunityBean jBean = null;
-
-		try {
-			jBean = new OtherJobOpportunityBean();
-			jBean.setId(rs.getInt("JOB_ID"));
-			jBean.setEndDate(new java.util.Date(rs.getTimestamp("CLOSING_DATE").getTime()));
-			jBean.setFilename(rs.getString("FILENAME"));
-			jBean.setTitle(rs.getString("JOB_TITLE"));
-			jBean.setPostingType(PostingType.get(Integer.parseInt(rs.getString("INTERNAL_ONLY"))));
-			if(rs.getTimestamp("CANCELLED_DATE")!=null)
-			{
-			jBean.setCancelledDate(new java.util.Date(rs.getTimestamp("CANCELLED_DATE").getTime()));
-			}
-			jBean.setCancelled_reason(rs.getString("CANCELLED_REASON"));
-			jBean.setCancelled_by(rs.getString("CANCELLED_BY"));
-			try {
-				if (rs.getInt("REGION_ID") == 0)
-					jBean.setRegion(RegionManager.getRegionBean(5)); //district wide
-				else
-					jBean.setRegion(RegionManager.getRegionBean(rs.getInt("REGION_ID")));
-			}
-			catch (Exception e) {}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			jBean = null;
-		}
-
-		return jBean;
-	}
-	
 	public static void deleteOtherJobOpportunityBean(Integer job_id) throws JobOpportunityException {
 
 		Connection con = null;
@@ -350,7 +273,7 @@ public class OtherJobOpportunityManager {
 			// get the opportunity info
 			stat = con.prepareCall("begin awsd_user.personnel_jobs_pkg.delete_job_other(?); end;");
 			stat.setInt(1, job_id);
-			
+
 			stat.execute();
 		}
 		catch (SQLException e) {
@@ -368,7 +291,9 @@ public class OtherJobOpportunityManager {
 			catch (Exception e) {}
 		}
 	}
-	public static void cancelOtherJobOpportunityBean(Integer job_id,String reason,String userid) throws JobOpportunityException {
+
+	public static void cancelOtherJobOpportunityBean(Integer job_id, String reason, String userid)
+			throws JobOpportunityException {
 
 		Connection con = null;
 		CallableStatement stat = null;
@@ -377,15 +302,12 @@ public class OtherJobOpportunityManager {
 			con = DAOUtils.getConnection();
 			con.setAutoCommit(false);
 
-			
-			
-			
 			// get the opportunity info
 			stat = con.prepareCall("begin awsd_user.personnel_jobs_pkg.cancel_job_other(?,?,?); end;");
 			stat.setString(1, userid);
 			stat.setString(2, reason);
 			stat.setInt(3, job_id);
-			
+
 			stat.execute();
 		}
 		catch (SQLException e) {
@@ -403,20 +325,19 @@ public class OtherJobOpportunityManager {
 			catch (Exception e) {}
 		}
 	}
-	
-	public static OtherJobOpportunityBean[] getOtherJobOpportunityBeanByRegion(int region_id) throws JobOpportunityException {
 
-		
-		Vector v_opps = null;
+	public static OtherJobOpportunityBean[] getOtherJobOpportunityBeanByRegion(int region_id)
+			throws JobOpportunityException {
+
+		Vector<OtherJobOpportunityBean> v_opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			
 
-			v_opps = new Vector(5);
+			v_opps = new Vector<OtherJobOpportunityBean>(5);
 
 			con = DAOUtils.getConnection();
 
@@ -454,19 +375,18 @@ public class OtherJobOpportunityManager {
 
 		return (OtherJobOpportunityBean[]) v_opps.toArray(new OtherJobOpportunityBean[0]);
 	}
+
 	public static OtherJobOpportunityBean[] getOtherJobOpportunityBeanByZone(int zone_id) throws JobOpportunityException {
 
-		
-		Vector v_opps = null;
+		Vector<OtherJobOpportunityBean> v_opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			
 
-			v_opps = new Vector(5);
+			v_opps = new Vector<OtherJobOpportunityBean>(5);
 
 			con = DAOUtils.getConnection();
 
@@ -503,8 +423,10 @@ public class OtherJobOpportunityManager {
 		}
 
 		return (OtherJobOpportunityBean[]) v_opps.toArray(new OtherJobOpportunityBean[0]);
-	}	
-	public static OtherJobOpportunityBean updateOtherJobOpportunityBean(OtherJobOpportunityBean jbean) throws JobOpportunityException {
+	}
+
+	public static OtherJobOpportunityBean updateOtherJobOpportunityBean(OtherJobOpportunityBean jbean)
+			throws JobOpportunityException {
 
 		Connection con = null;
 		CallableStatement stat = null;
@@ -518,14 +440,12 @@ public class OtherJobOpportunityManager {
 			stat.setInt(1, jbean.getId());
 			stat.setString(2, jbean.getTitle());
 			stat.setTimestamp(3, new Timestamp(jbean.getEndDate().getTime()));
-			stat.setString(4,jbean.getFilename());
+			stat.setString(4, jbean.getFilename());
 			stat.setString(5, Integer.toString(jbean.getPostingType().getValue()));
 			stat.setInt(6, jbean.getRegion().getId());
 
 			stat.execute();
 			stat.close();
-
-			
 
 			con.commit();
 		}
@@ -552,18 +472,19 @@ public class OtherJobOpportunityManager {
 
 		return jbean;
 	}
-	
-	public static TreeMap getInternalOnlyOtherJobOpportunityBeans2() throws JobOpportunityException {
 
-		TreeMap regions = null;
-		Vector opps = null;
+	public static TreeMap<String, Vector<OtherJobOpportunityBean>> getInternalOnlyOtherJobOpportunityBeans2()
+			throws JobOpportunityException {
+
+		TreeMap<String, Vector<OtherJobOpportunityBean>> regions = null;
+		Vector<OtherJobOpportunityBean> opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			regions = new TreeMap();
+			regions = new TreeMap<String, Vector<OtherJobOpportunityBean>>();
 
 			con = DAOUtils.getConnection();
 
@@ -576,13 +497,30 @@ public class OtherJobOpportunityManager {
 			while (rs.next()) {
 				jBean = createOtherJobOpportunityBean(rs);
 
-				if (jBean.isExternalOnly())
+				if (jBean.isExternalOnly()) {
 					continue;
+				}
+
+				if ((jBean.getRegion() == null)) {
+
+					new AlertBean(new RegionException("OtherJobOpportunityBean[id=" + jBean.getId()
+							+ "] has no assigned REGION"));
+
+					continue;
+				}
+				else if ((jBean.getRegion().getZone() == null)) {
+
+					new AlertBean(new RegionException("OtherJobOpportunityBean[id=" + jBean.getId() + ", RegionID="
+							+ jBean.getRegion().getId() + "] has no assigned ZONE"));
+
+					continue;
+				}
 
 				if (regions.containsKey(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName()))
-					opps = (Vector) regions.get(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName());
+					opps = (Vector<OtherJobOpportunityBean>) regions.get(
+							jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName());
 				else {
-					opps = new Vector();
+					opps = new Vector<OtherJobOpportunityBean>();
 					regions.put(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName(), opps);
 				}
 				opps.add(jBean);
@@ -609,17 +547,19 @@ public class OtherJobOpportunityManager {
 
 		return regions;
 	}
-	public static TreeMap getExternalOnlyOtherJobOpportunityBeans2() throws JobOpportunityException {
 
-		TreeMap regions = null;
-		Vector opps = null;
+	public static TreeMap<String, Vector<OtherJobOpportunityBean>> getExternalOnlyOtherJobOpportunityBeans2()
+			throws JobOpportunityException {
+
+		TreeMap<String, Vector<OtherJobOpportunityBean>> regions = null;
+		Vector<OtherJobOpportunityBean> opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			regions = new TreeMap();
+			regions = new TreeMap<String, Vector<OtherJobOpportunityBean>>();
 
 			con = DAOUtils.getConnection();
 
@@ -632,19 +572,36 @@ public class OtherJobOpportunityManager {
 			while (rs.next()) {
 				jBean = createOtherJobOpportunityBean(rs);
 
-				if (jBean.isInternalOnly())
+				if (jBean.isInternalOnly()) {
 					continue;
+				}
+
+				if ((jBean.getRegion() == null)) {
+
+					new AlertBean(new RegionException("OtherJobOpportunityBean[id=" + jBean.getId()
+							+ "] has no assigned REGION"));
+
+					continue;
+				}
+				else if ((jBean.getRegion().getZone() == null)) {
+
+					new AlertBean(new RegionException("OtherJobOpportunityBean[id=" + jBean.getId() + ", RegionID="
+							+ jBean.getRegion().getId() + "] has no assigned ZONE"));
+
+					continue;
+				}
 
 				if (regions.containsKey(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName()))
-					opps = (Vector) regions.get(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName());
+					opps = (Vector<OtherJobOpportunityBean>) regions.get(
+							jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName());
 				else {
-					opps = new Vector();
+					opps = new Vector<OtherJobOpportunityBean>();
 					regions.put(jBean.getRegion().getZone().getZoneName() + " - " + jBean.getRegion().getName(), opps);
 				}
 				opps.add(jBean);
 			}
 		}
-		catch (SQLException e) {
+		catch (Exception e) {
 			System.err.println("JobOpportunityManager.getJobOpportunityBeans(boolean): " + e);
 			throw new JobOpportunityException("Can not extract JobOpportunityBean from DB.", e);
 		}
@@ -665,19 +622,19 @@ public class OtherJobOpportunityManager {
 
 		return regions;
 	}
-	public static OtherJobOpportunityBean[] getApplicantAppliedOpenJobOpportunityBeans(String sin) throws JobOpportunityException {
 
-		
-		Vector v_opps = null;
+	public static OtherJobOpportunityBean[] getApplicantAppliedOpenJobOpportunityBeans(String sin)
+			throws JobOpportunityException {
+
+		Vector<OtherJobOpportunityBean> v_opps = null;
 		OtherJobOpportunityBean jBean = null;
 		Connection con = null;
 		CallableStatement stat = null;
 		ResultSet rs = null;
 
 		try {
-			
 
-			v_opps = new Vector(5);
+			v_opps = new Vector<OtherJobOpportunityBean>(5);
 
 			con = DAOUtils.getConnection();
 
@@ -714,5 +671,37 @@ public class OtherJobOpportunityManager {
 		}
 
 		return (OtherJobOpportunityBean[]) v_opps.toArray(new OtherJobOpportunityBean[0]);
-	}	
+	}
+
+	public static OtherJobOpportunityBean createOtherJobOpportunityBean(ResultSet rs) {
+
+		OtherJobOpportunityBean jBean = null;
+
+		try {
+			jBean = new OtherJobOpportunityBean();
+			jBean.setId(rs.getInt("JOB_ID"));
+			jBean.setEndDate(new java.util.Date(rs.getTimestamp("CLOSING_DATE").getTime()));
+			jBean.setFilename(rs.getString("FILENAME"));
+			jBean.setTitle(rs.getString("JOB_TITLE"));
+			jBean.setPostingType(PostingType.get(Integer.parseInt(rs.getString("INTERNAL_ONLY"))));
+			if (rs.getTimestamp("CANCELLED_DATE") != null) {
+				jBean.setCancelledDate(new java.util.Date(rs.getTimestamp("CANCELLED_DATE").getTime()));
+			}
+			jBean.setCancelled_reason(rs.getString("CANCELLED_REASON"));
+			jBean.setCancelled_by(rs.getString("CANCELLED_BY"));
+			try {
+				if (rs.getInt("REGION_ID") == 0)
+					jBean.setRegion(RegionManager.getRegionBean(5)); //district wide
+				else
+					jBean.setRegion(RegionManager.getRegionBean(rs.getInt("REGION_ID")));
+			}
+			catch (Exception e) {}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			jBean = null;
+		}
+
+		return jBean;
+	}
 }
