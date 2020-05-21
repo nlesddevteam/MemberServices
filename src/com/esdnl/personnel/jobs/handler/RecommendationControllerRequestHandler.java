7a46075f3323453c8b08962390005780cec25e50
Chris Crane <chriscrane@nlesd.ca>
2020-05-21 14:31:12 -0230

    Queue offer notification sent on friday after 12PM to Monday.

--- src/com/esdnl/personnel/jobs/handler/RecommendationControllerRequestHandler.java
+++ src/com/esdnl/personnel/jobs/handler/RecommendationControllerRequestHandler.java
@@ -497,7 +497,12 @@ public class RecommendationControllerRequestHandler extends RequestHandlerImpl {
 			cal.clear(Calendar.MINUTE);
 
 			if (cal.get(Calendar.HOUR_OF_DAY) >= 12) {
-				cal.add(Calendar.DAY_OF_MONTH, 1);
+				if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
+					cal.add(Calendar.DAY_OF_MONTH, 3);
+				}
+				else {
+					cal.add(Calendar.DAY_OF_MONTH, 1);
+				}
 			}
 			cal.set(Calendar.HOUR_OF_DAY, 12);
 
