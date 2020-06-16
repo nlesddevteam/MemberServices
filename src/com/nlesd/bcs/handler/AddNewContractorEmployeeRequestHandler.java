72abe22e959b88b5a76421d57699816801a5063f
rdbatten <rodneybatten@nlesd.ca>
2020-06-16 14:16:07 -0230

    added check for unique dl when adding employee admin/contractor

--- src/com/nlesd/bcs/handler/AddNewContractorEmployeeRequestHandler.java
+++ src/com/nlesd/bcs/handler/AddNewContractorEmployeeRequestHandler.java
@@ -41,187 +41,192 @@ public class AddNewContractorEmployeeRequestHandler extends BCSApplicationReques
 		String message="UPDATED";
 		if (validate_form()) {
 		try {
-				
-				SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-				BussingContractorBean bcbean = (BussingContractorBean) request.getSession(false).getAttribute("CONTRACTOR");
-				vbean.setContractorId(bcbean.getId());
-				vbean.setEmployeePosition(form.getInt("employeeposition"));
-				vbean.setStartDate(form.get("vmonth") + "-" + form.get("vyear"));
-				vbean.setContinuousService(form.get("continuousservice"));
-				vbean.setFirstName(form.get("firstname"));
-				vbean.setLastName(form.get("lastname"));
-				vbean.setMiddleName(form.get("middlename"));
-				vbean.setAddress1(form.get("address1"));
-				vbean.setAddress2(form.get("address2"));
-				vbean.setCity(form.get("city"));
-				vbean.setProvince(form.get("province"));
-				vbean.setPostalCode(form.get("postalcode"));
-				vbean.setHomePhone(form.get("homephone"));
-				vbean.setCellPhone(form.get("cellphone"));
-				vbean.setEmail(form.get("email"));
-				vbean.setDlNumber(form.get("dlnumber"));
-				if(form.get("dlexpirydate").isEmpty())
-				{
-					vbean.setDlExpiryDate(null);
-				}else{
-					vbean.setDlExpiryDate(sdf.parse(form.get("dlexpirydate").toString()));
+				//check to see if it is a driver position and if the dl number not in use
+				if(form.getInt("employeeposition") == 20 && BussingContractorEmployeeManager.checkEmployeeDLNumber(form.get("dlnumber"))) {
+					message="Employee record on file with identical Driver License Number";
+				}else {
+					SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+					BussingContractorBean bcbean = (BussingContractorBean) request.getSession(false).getAttribute("CONTRACTOR");
+					vbean.setContractorId(bcbean.getId());
+					vbean.setEmployeePosition(form.getInt("employeeposition"));
+					vbean.setStartDate(form.get("vmonth") + "-" + form.get("vyear"));
+					vbean.setContinuousService(form.get("continuousservice"));
+					vbean.setFirstName(form.get("firstname"));
+					vbean.setLastName(form.get("lastname"));
+					vbean.setMiddleName(form.get("middlename"));
+					vbean.setAddress1(form.get("address1"));
+					vbean.setAddress2(form.get("address2"));
+					vbean.setCity(form.get("city"));
+					vbean.setProvince(form.get("province"));
+					vbean.setPostalCode(form.get("postalcode"));
+					vbean.setHomePhone(form.get("homephone"));
+					vbean.setCellPhone(form.get("cellphone"));
+					vbean.setEmail(form.get("email"));
+					vbean.setDlNumber(form.get("dlnumber"));
+					if(form.get("dlexpirydate").isEmpty())
+					{
+						vbean.setDlExpiryDate(null);
+					}else{
+						vbean.setDlExpiryDate(sdf.parse(form.get("dlexpirydate").toString()));
+					}
+					vbean.setDlClass(form.getInt("dlclass"));
+					if(form.get("darundate").isEmpty())
+					{
+						vbean.setDaRunDate(null);
+					}else{
+						vbean.setDaRunDate(sdf.parse(form.get("darundate").toString()));
+					}
+					vbean.setDaConvictions(form.get("daconvictions"));
+					if(form.get("faexpirydate").isEmpty())
+					{
+						vbean.setFaExpiryDate(null);
+					}else{
+						vbean.setFaExpiryDate(sdf.parse(form.get("faexpirydate").toString()));
+					}
+					if(form.get("pccdate").isEmpty())
+					{
+						vbean.setPccDate(null);
+					}else{
+						vbean.setPccDate(sdf.parse(form.get("pccdate").toString()));
+					}				
+					if(form.get("scadate").isEmpty())
+					{
+						vbean.setScaDate(null);
+					}else{
+						vbean.setScaDate(sdf.parse(form.get("scadate").toString()));
+					}
+					if(form.get("prcvsqdate").isEmpty())
+					{
+						vbean.setPrcvsqDate(null);
+					}else{
+						vbean.setPrcvsqDate(sdf.parse(form.get("prcvsqdate").toString()));
+					}
+					vbean.setFindingsOfGuilt(form.get("findingsofguilt"));
+					if(form.get("birthdate").isEmpty())
+					{
+						vbean.setBirthDate(null);
+					}else{
+						vbean.setBirthDate(sdf.parse(form.get("birthdate").toString()));
+					}
+					vbean.setDaSuspensions(form.get("dasuspensions"));
+					vbean.setDaAccidents(form.get("daaccidents"));
+					//now we do the documents
+					String filelocation="/BCS/documents/employeedocs/";
+					String docfilename = "";
+					if(form.getUploadFile("dlfront").getFileSize() > 0){
+						docfilename=save_file("dlfront", filelocation);
+						vbean.setDlFront(docfilename);
+					}
+					if(form.getUploadFile("dlback").getFileSize() > 0){
+						docfilename=save_file("dlback", filelocation);
+						vbean.setDlBack(docfilename);
+					}
+					if(form.getUploadFile("dadocument").getFileSize() > 0){
+						docfilename=save_file("dadocument", filelocation);
+						vbean.setDaDocument(docfilename);
+					}
+					if(form.getUploadFile("fadocument").getFileSize() > 0){
+						docfilename=save_file("fadocument", filelocation);
+						vbean.setFaDocument(docfilename);
+					}
+					if(form.getUploadFile("prcvsqdocument").getFileSize() > 0){
+						docfilename=save_file("prcvsqdocument", filelocation);
+						vbean.setPrcvsqDocument(docfilename);
+					}
+					if(form.getUploadFile("pccdocument").getFileSize() > 0){
+						docfilename=save_file("pccdocument", filelocation);
+						vbean.setPccDocument(docfilename);
+					}
+					if(form.getUploadFile("scadocument").getFileSize() > 0){
+						docfilename=save_file("scadocument", filelocation);
+						vbean.setScaDocument(docfilename);
+					}
+					vbean.setStatus(EmployeeStatusConstant.NOTREVIEWED.getValue());
+					
+					//now we add the record
+					BussingContractorEmployeeManager.addBussingContractorEmployee(vbean);
+					//now that we have an id we can update the file history objects
+					if(!(vbean.getDlFront() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getDlFront());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(1);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getDlBack() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getDlBack());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(2);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getDaDocument() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getDaDocument());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(3);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getFaDocument() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getFaDocument());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(4);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getPrcvsqDocument() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getPrcvsqDocument());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(5);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getPccDocument() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getPccDocument());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(6);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					if(!(vbean.getScaDocument() ==  null)) {
+						//now we save document history record
+						FileHistoryBean fhb = new FileHistoryBean();
+						fhb.setFileName(vbean.getScaDocument());
+						fhb.setFileAction("UPLOADED");
+						fhb.setActionBy(bcbean.getContractorName());
+						fhb.setParentObjectId(vbean.getId());
+						fhb.setParentObjectType(7);
+						FileHistoryManager.addFileHistory(fhb);
+					}
+					//update audit trail
+					AuditTrailBean atbean = new AuditTrailBean();
+					atbean.setEntryType(EntryTypeConstant.CONTRACTOREMPLOYEEADDED);
+					atbean.setEntryId(vbean.getId());
+					atbean.setEntryTable(EntryTableConstant.CONTRACTOREMPLOYEE);
+					DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
+					atbean.setEntryNotes("Contractor employee (" + vbean.getLastName() + "," + vbean.getFirstName() + ") added on  " + dateTimeInstance.format(Calendar.getInstance().getTime()));
+					atbean.setContractorId(vbean.getContractorId());
+					AuditTrailManager.addAuditTrail(atbean);
 				}
-				vbean.setDlClass(form.getInt("dlclass"));
-				if(form.get("darundate").isEmpty())
-				{
-					vbean.setDaRunDate(null);
-				}else{
-					vbean.setDaRunDate(sdf.parse(form.get("darundate").toString()));
-				}
-				vbean.setDaConvictions(form.get("daconvictions"));
-				if(form.get("faexpirydate").isEmpty())
-				{
-					vbean.setFaExpiryDate(null);
-				}else{
-					vbean.setFaExpiryDate(sdf.parse(form.get("faexpirydate").toString()));
-				}
-				if(form.get("pccdate").isEmpty())
-				{
-					vbean.setPccDate(null);
-				}else{
-					vbean.setPccDate(sdf.parse(form.get("pccdate").toString()));
-				}				
-				if(form.get("scadate").isEmpty())
-				{
-					vbean.setScaDate(null);
-				}else{
-					vbean.setScaDate(sdf.parse(form.get("scadate").toString()));
-				}
-				if(form.get("prcvsqdate").isEmpty())
-				{
-					vbean.setPrcvsqDate(null);
-				}else{
-					vbean.setPrcvsqDate(sdf.parse(form.get("prcvsqdate").toString()));
-				}
-				vbean.setFindingsOfGuilt(form.get("findingsofguilt"));
-				if(form.get("birthdate").isEmpty())
-				{
-					vbean.setBirthDate(null);
-				}else{
-					vbean.setBirthDate(sdf.parse(form.get("birthdate").toString()));
-				}
-				vbean.setDaSuspensions(form.get("dasuspensions"));
-				vbean.setDaAccidents(form.get("daaccidents"));
-				//now we do the documents
-				String filelocation="/BCS/documents/employeedocs/";
-				String docfilename = "";
-				if(form.getUploadFile("dlfront").getFileSize() > 0){
-					docfilename=save_file("dlfront", filelocation);
-					vbean.setDlFront(docfilename);
-				}
-				if(form.getUploadFile("dlback").getFileSize() > 0){
-					docfilename=save_file("dlback", filelocation);
-					vbean.setDlBack(docfilename);
-				}
-				if(form.getUploadFile("dadocument").getFileSize() > 0){
-					docfilename=save_file("dadocument", filelocation);
-					vbean.setDaDocument(docfilename);
-				}
-				if(form.getUploadFile("fadocument").getFileSize() > 0){
-					docfilename=save_file("fadocument", filelocation);
-					vbean.setFaDocument(docfilename);
-				}
-				if(form.getUploadFile("prcvsqdocument").getFileSize() > 0){
-					docfilename=save_file("prcvsqdocument", filelocation);
-					vbean.setPrcvsqDocument(docfilename);
-				}
-				if(form.getUploadFile("pccdocument").getFileSize() > 0){
-					docfilename=save_file("pccdocument", filelocation);
-					vbean.setPccDocument(docfilename);
-				}
-				if(form.getUploadFile("scadocument").getFileSize() > 0){
-					docfilename=save_file("scadocument", filelocation);
-					vbean.setScaDocument(docfilename);
-				}
-				vbean.setStatus(EmployeeStatusConstant.NOTREVIEWED.getValue());
-				
-				//now we add the record
-				BussingContractorEmployeeManager.addBussingContractorEmployee(vbean);
-				//now that we have an id we can update the file history objects
-				if(!(vbean.getDlFront() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getDlFront());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(1);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getDlBack() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getDlBack());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(2);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getDaDocument() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getDaDocument());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(3);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getFaDocument() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getFaDocument());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(4);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getPrcvsqDocument() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getPrcvsqDocument());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(5);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getPccDocument() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getPccDocument());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(6);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				if(!(vbean.getScaDocument() ==  null)) {
-					//now we save document history record
-					FileHistoryBean fhb = new FileHistoryBean();
-					fhb.setFileName(vbean.getScaDocument());
-					fhb.setFileAction("UPLOADED");
-					fhb.setActionBy(bcbean.getContractorName());
-					fhb.setParentObjectId(vbean.getId());
-					fhb.setParentObjectType(7);
-					FileHistoryManager.addFileHistory(fhb);
-				}
-				//update audit trail
-				AuditTrailBean atbean = new AuditTrailBean();
-				atbean.setEntryType(EntryTypeConstant.CONTRACTOREMPLOYEEADDED);
-				atbean.setEntryId(vbean.getId());
-				atbean.setEntryTable(EntryTableConstant.CONTRACTOREMPLOYEE);
-				DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
-				atbean.setEntryNotes("Contractor employee (" + vbean.getLastName() + "," + vbean.getFirstName() + ") added on  " + dateTimeInstance.format(Calendar.getInstance().getTime()));
-				atbean.setContractorId(vbean.getContractorId());
-				AuditTrailManager.addAuditTrail(atbean);
+
 				
 				}catch(Exception e){
 					message = e.getMessage();
