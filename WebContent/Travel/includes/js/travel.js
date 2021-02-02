838f079476d23a5d09aa699a4c4ad0bccc92a69e
geofftaylor <geofftaylor@EE-BO-F2-233.district.nlesd.ca>
2021-02-02 10:15:42 -0330

    Travel Fix for decimal km

--- WebContent/Travel/includes/js/travel.js
+++ WebContent/Travel/includes/js/travel.js
@@ -19,8 +19,9 @@ $( '.dropdown-menu .dropdown-toggle' ).on('click', function() {
 function isNumber(evt) {
         evt = (evt) ? evt : window.event;
         var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if ( (charCode > 31 && charCode < 48) || charCode > 57) {
-          //  return false;
+        if ( (charCode > 31 && charCode < 48) || charCode > 57 || charCode == 46) {
+	   $(".decOnlyMsg").html("<span style='color:Red;'><b>ERROR:</b> Please round to nearest kilometer. No decimals.</span>").css("display","block").delay(4000).fadeOut();
+         return false;
         }
         return true;
     }
@@ -29,15 +30,12 @@ function isNumberDec(evt)
    var charCode = (evt.which) ? evt.which : evt.keyCode;
    if (charCode != 46 && charCode > 31 
      && (charCode < 48 || charCode > 57)) {
-    //  return false;
+   return false;
    }
    return true;
 }  
 
-
 $(document).ready(function () {	
-
-
 
 
 //CHANGE FONT SIZE		   	
